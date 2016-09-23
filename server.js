64ced49468bbb3e7930129c6ae7aee79cba64221
krithika29 <krithika18.ece@gmail.com>
2016-09-23 20:17:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/Articleone',function(req,res){
-    res.sendFile(path.join(null,'ui','article-one.html'));
+    res.sendFile(path.join(__dirname,'ui','article-one.html'));
+    console.log(__dirname);
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
